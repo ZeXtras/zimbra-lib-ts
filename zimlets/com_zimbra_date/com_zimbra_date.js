2c5f918e4086efb8a4a18537b4e06989a7119bbb
Michele Olivo <mo@zextras.com>
2017-03-11 16:03:09 +0100

    Fixed importers for com_zimbra_attachmail and com_zimbra_date

--- zimlets/com_zimbra_date/com_zimbra_date.js
+++ zimlets/com_zimbra_date/com_zimbra_date.js
@@ -1,5 +1,5 @@
 define(["require", "exports"], function (require, exports) {
   "use strict";
   exports.Com_Zimbra_Date = (typeof Com_Zimbra_Date !== "undefined") ? Com_Zimbra_Date : void 0;
-  exports.com_zimbra_date = com_zimbra_date;
+  exports.com_zimbra_date = (typeof com_zimbra_date !== "undefined") ? com_zimbra_date : void 0;
 });
