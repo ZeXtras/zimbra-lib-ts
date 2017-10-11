dac535bb994766e528511e2f9e2995f60d43175e
Stefano Grasselli <sg@zextras.com>
2017-10-11 12:11:53 +0200

    Fixed declaration of object that contain arrays of constructors

--- zimbraAdmin/common/ZaItem.ts
+++ zimbraAdmin/common/ZaItem.ts
@@ -43,15 +43,15 @@ export class ZaItem extends ZaModel {
   public static A_zimbraACE: string;
   public static A_zimbraCreateTimestamp: string;
 
-  public static loadMethods: {[className: string]: Array<typeof className>};
-  public static initMethods: {[className: string]: Array<typeof className>};
-  public static modifyMethods: {[className: string]: Array<typeof className>};
-  public static modifyMethodsExt: {[className: string]: Array<typeof className>};
-  public static createMethods: {[className: string]: Array<typeof className>};
-  public static removeMethods: {[className: string]: Array<typeof className>};
-  public static modelExtensions: {[className: string]: Array<typeof className>};
-  public static getRelatedMethods: {[className: string]: Array<typeof className>};
-  public static ObjectModifiers: {[className: string]: Array<typeof className>};
+  public static loadMethods: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
+  public static initMethods: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
+  public static modifyMethods: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
+  public static modifyMethodsExt: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
+  public static createMethods: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
+  public static removeMethods: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
+  public static modelExtensions: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
+  public static getRelatedMethods: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
+  public static ObjectModifiers: {[className: string]: Array<new (...args: any[]) => any/*className*/>};
 
   public rightsLoaded: boolean;
   public id: string; // TODO: Investigate
